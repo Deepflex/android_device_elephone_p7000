d5961c516750a2d5b0a51d3f53d3cfe8dd41840c
Maxim <hqdevnews@gmail.com>
2016-04-08 17:46:34 +0700

    RC2
    RC2

--- device.mk
+++ device.mk
@@ -95,7 +95,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_COPY_FILES += \
      $(LOCAL_PATH)/configs/mtk-kpd.kl:system/usr/keylayout/mtk-kpd.kl \
      $(LOCAL_PATH)/configs/ACCDET.kl:system/usr/keylayout/ACCDET.kl\
-     $(LOCAL_PATH)/configs/ACCDET.kl:system/usr/keylayout/Generic.kl\
+     $(LOCAL_PATH)/configs/Generic.kl:system/usr/keylayout/Generic.kl\
      $(LOCAL_PATH)/configs/AVRCP.kl:system/usr/keylayout/AVRCP.kl    
 
 # Ramdisk
